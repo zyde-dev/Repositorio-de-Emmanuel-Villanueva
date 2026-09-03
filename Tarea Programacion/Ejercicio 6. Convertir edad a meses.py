de8b7d4dc3d1dd582e8ac94bad8d8973674d4baa
emmanuelvillanueva121-cloud <emmanuelvillanueva121@gmail.com>
2026-09-03 17:30:39 -0500

    Delete Tarea Programacion/Ejercicio 6. Convertir edad a meses.py

--- Tarea Programacion/Ejercicio 6. Convertir edad a meses.py
+++
@@ -1,9 +0,0 @@
-#Solicitar la edad de una persona y calcular aproximadamente cuántos meses ha vivido
-#Ejemplo:
-#Edad: 20
-#Has vivido aproximadamente 240 meses
-
-edad = float(input("Ingrese su edad: "))
-meses_vividos = edad*12
-print("//")
-print("Has vivido aproximadamente: ", meses_vividos, "meses")
