051e1e6a54243cea860a752f8600dd687abadd99
emmanuelvillanueva121-cloud <emmanuelvillanueva121@gmail.com>
2026-09-03 17:17:48 -0500

    Delete Tarea Programacion/Taller Programacion Parte 3 y 4/Ejercicio13.py

--- Tarea Programacion/Taller Programacion Parte 3 y 4/Ejercicio13.py
+++
@@ -1,5 +0,0 @@
-nombre = input("Ingrese su nombre: ")
-carrera = input("Ingrese su carrera: ")
-universidad = input("Ingrese su universidad: ")
-
-print("Hola",nombre,"estuias",carrera,"en la universidad ",universidad)
